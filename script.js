9cc7af53799c0c9c9a9de58254b91b7d32f698a6
lord-cyclone100 <sang66deb@gmail.com>
2023-12-30 22:10:23 +0530

    Added the logic

--- script.js
+++ script.js
@@ -10,6 +10,41 @@ obj = [{
 },{
     quote: "Your brain is the most important organ in your body, and what happens in it determines what you think and feel, say and do.",
     speaker: "~ Rick Hanson"
-}]
+},{
+    quote: "If your actions inspire others to dream more, learn more, do more and become more, you are a leader.",
+    speaker: "~ John Quincy Adams",
+},{
+    quote: "There is only one way to avoid criticism: do nothing, say nothing, and be nothing.",  
+    speaker: "~ Aristotle",
+},{
+    quote: "Only those who will risk going too far can possibly find out how far one can go.", 
+    speaker: "~ T. S. Eliot",
+},{
+    quote:  "Only those who play to win. Only those who risk to win. History favors risk-takers. Forgets the timid. Everything else is commentary.",
+    speaker: "~ Iveta Cherneva",
+},{
+    quote: "If you don’t build your dream, someone else will hire you to help them build theirs.", 
+    speaker: "~ Dhirubhai Ambani",
+}
+]
 
-console.log(obj);
+console.log(obj[0].speaker);
+
+let nextButton = document.querySelector('button');
+let quote = document.querySelector('.quote');
+let speaker = document.querySelector('.speaker')
+
+let n = 0
+nextButton.addEventListener('click', function(){
+    let num = Math.round(Math.random()*obj.length);
+    if (num != n){
+        quote.innerText = (obj[num].quote);
+        speaker.innerText = (obj[num].speaker);
+    }
+    else{
+        num ++;
+        quote.innerText = (obj[num].quote);
+        speaker.innerText = (obj[num].speaker);
+    }
+    n = num;
+})
