6365d7871787b54957a08d210f34992e70f23b53
lord-cyclone100 <sang66deb@gmail.com>
2023-12-30 17:55:22 +0530

    Added initial files

---
+++ script.js
@@ -0,0 +1,15 @@
+obj = [{
+    quote: "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear.",
+    speaker: "~ Nelson Mandela"
+},{
+    quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+    speaker: "~ Oprah Winfrey"
+},{
+    quote: "The real test is not whether you avoid this failure, because you won't. It's whether you let it harden or shame you into inaction, or whether you learn from it; whether you choose to persevere.",
+    speaker: "~ Barack Obama"
+},{
+    quote: "Your brain is the most important organ in your body, and what happens in it determines what you think and feel, say and do.",
+    speaker: "~ Rick Hanson"
+}]
+
+console.log(obj);
